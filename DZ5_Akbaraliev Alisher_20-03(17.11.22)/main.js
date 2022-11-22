3276309dc161c5d56fc848646a79db4313efc847
alisher <106680187+AkbaralievAlisher@users.noreply.github.com>
2022-11-23 02:07:29 +0600

    add new DZ5 > ES6

---
+++ DZ5_Akbaraliev Alisher_20-03(17.11.22)/main.js
@@ -0,0 +1,39 @@
+const input = document.getElementById('input')
+const button = document.getElementById('create_button')
+const list = document.getElementById('todo__list')
+console.log(button);
+
+let todo = e =>
+{
+    if (input.value.trim() != '') {
+        const div = document.createElement("div")
+        const text = document.createElement("h3")
+        const btnsDiv = document.createElement("div")
+        const btnDelete = document.createElement("button")
+        const btnEdit = document.createElement("button")
+        btnDelete.setAttribute("class", "btn")
+        btnEdit.setAttribute("class", "btn")
+        btnsDiv.setAttribute("class", "btns")
+        div.setAttribute("class", "block_text")
+        btnDelete.innerText='delete'
+        btnEdit.innerText='edit'
+        text.innerText = input.value
+        list.append(div)
+        div.append(text,btnsDiv)
+        btnsDiv.append(btnDelete,btnEdit)
+        input.value = ""
+        btnDelete.onclick=()=>{div.remove()}
+        btnEdit.onclick=()=>{
+            let p=prompt()
+            if(p.trim()!=''){
+            text.innerText = p
+        }}
+    }
+    else input.value = ""
+}
+
+button.onclick = () => todo()
+input.addEventListener('keydown', e =>
+{
+    if (e.keyCode === 13) todo()
+})
